aa71b148502fdf80edd3399d1dde5dda9160a702
Russell Soza <endosoza1@gmail.com>
2024-06-19 23:02:42 -0600

    Agregando DBContext y modelo de usuario

---
+++ Birthflow_Service.Application/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Birthflow_Service.Application.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public required string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public required string Contraseña { get; set; }
+    }
+}
---
+++ Birthflow_Service.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Birthflow_Service.Domain.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() { }
+    }
+}
