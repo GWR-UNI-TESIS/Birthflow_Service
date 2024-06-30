be21f9683bc4f2c482543ace9e1a2d1549bdc017
gersonMD <medranodavilag04@gmail.com>
2024-06-29 18:33:58 -0600

    Nueva migracion: Se hicieron modificaciones en la entidad usuario

---
+++ Birthflow_Service.Infraestructure/Migrations/20240630002149_Eliminando_Columna_RolId_y_CreatedBy_En_UsuarioEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Birthflow_Service.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Eliminando_Columna_RolId_y_CreatedBy_En_UsuarioEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "Auth",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "RolId",
+                schema: "Auth",
+                table: "Usuario");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedBy",
+                schema: "Auth",
+                table: "Usuario",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "RolId",
+                schema: "Auth",
+                table: "Usuario",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
---
+++ Birthflow_Service.Infraestructure/Migrations/20240630003015_Agregando_columnas_de_auditoria_en_UsuarioEntity.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Birthflow_Service.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregando_columnas_de_auditoria_en_UsuarioEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                schema: "Auth",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeletedBy",
+                schema: "Auth",
+                table: "Usuario",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                schema: "Auth",
+                table: "Usuario",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "Auth",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                schema: "Auth",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                schema: "Auth",
+                table: "Usuario");
+        }
+    }
+}
